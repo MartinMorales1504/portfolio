9a8686d68a08d4dfc7a20bf803e0d8098980b2e3
MartinMorales1504 <martingabrielmorales@yahoo.com>
2022-10-20 21:42:36 -0300

    real first commit

---
+++ src/components/portfolio/about.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import s from './about.module.scss'
+import { motion } from 'framer-motion'
+
+const About = () => {
+
+    return (
+        <div className={s.container}>
+            <motion.div className={s.joinButton}>
+                <h2
+                    animate={{ fontSize: 40, color: '#ff2994' }}
+                >
+                    Hola como estas
+                </h2>
+            </motion.div>
+        </div>
+    )
+}
+
+export default About
